f9238592036b2e1370c4b384533331b9f143be45
Bogdan Barbulescu <bibscy@gmail.com>
2017-06-26 10:26:57 +0100

    first commit

---
+++ someTestTab/CustomController.swift
@@ -0,0 +1,35 @@
+//
+//  CustomController.swift
+//  someTestTab
+//
+//  Created by Bogdan Barbulescu on 26/06/2017.
+//  Copyright © 2017 vividapartments. All rights reserved.
+//
+
+import UIKit
+
+class CustomController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ someTestTab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  someTestTab
+//
+//  Created by Bogdan Barbulescu on 26/06/2017.
+//  Copyright © 2017 vividapartments. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
